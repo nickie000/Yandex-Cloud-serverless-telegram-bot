efc08b87cfebbf0ca3380d0052155271a9bf6c1e
Nikita <ngreshchuk@vk.com>
2023-05-31 03:29:21 +0500

    new file:   main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+import json
+import logging
+import os
+import matvey
+import common
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+token='6224320755:AAFulm89ICDa_dhR7DWRKo9L6N0ZVJlOGIA'
+
+# Logger initialization and logging level setting
+log = logging.getLogger(__name__)
+log.setLevel(os.environ.get('LOGGING_LEVEL', 'INFO').upper())
+
+
+async def process_event(event, dp: Dispatcher):
+    """
+    Converting an Yandex.Cloud functions event to an update and
+    handling tha update.
+    """
+
+    update = json.loads(event['body'])
+    log.debug('Update: ' + str(update))
+
+    Bot.set_current(dp.bot)
+    update = types.Update.to_object(update)
+    await dp.process_update(update)
+
+
+async def handler(event, context):
+    """Yandex.Cloud functions handler."""
+
+    if event['httpMethod'] == 'POST':
+        # Bot and dispatcher initialization
+        bot = Bot(token)
+        dp = Dispatcher(bot)
+        
+        await common.register_handlers(dp)
+        await matvey.register_handlers(dp)
+        await process_event(event, dp)
+
+        return {'statusCode': 200, 'body': 'ok'}
+    return {'statusCode': 405}
